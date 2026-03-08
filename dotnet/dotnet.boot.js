b91d2e2ab7ba7a2df019d6307b7781011d23293d
MrKWatkins <MrKWatkins@users.noreply.github.com>
2026-03-08 15:21:23 +0000

    Deployed 7c5fe2e with MkDocs version: 1.6.1

---
+++ dotnet/dotnet.boot.js
@@ -0,0 +1,125 @@
+export const config = {
+  "mainAssemblyName": "MrKWatkins.OakIO.Wasm.dll",
+  "resources": {
+    "jsModuleNative": [
+      {
+        "name": "dotnet.native.js"
+      }
+    ],
+    "jsModuleRuntime": [
+      {
+        "name": "dotnet.runtime.js"
+      }
+    ],
+    "jsModuleWorker": [
+      {
+        "name": "dotnet.native.worker.mjs"
+      }
+    ],
+    "wasmNative": [
+      {
+        "name": "dotnet.native.wasm"
+      }
+    ],
+    "assembly": [
+      {
+        "name": "MrKWatkins.BinaryPrimitives.dll",
+        "virtualPath": "MrKWatkins.BinaryPrimitives.dll"
+      },
+      {
+        "name": "MrKWatkins.OakIO.Commands.dll",
+        "virtualPath": "MrKWatkins.OakIO.Commands.dll"
+      },
+      {
+        "name": "MrKWatkins.OakIO.Wasm.dll",
+        "virtualPath": "MrKWatkins.OakIO.Wasm.dll"
+      },
+      {
+        "name": "MrKWatkins.OakIO.ZXSpectrum.dll",
+        "virtualPath": "MrKWatkins.OakIO.ZXSpectrum.dll"
+      },
+      {
+        "name": "MrKWatkins.OakIO.dll",
+        "virtualPath": "MrKWatkins.OakIO.dll"
+      },
+      {
+        "name": "System.Collections.Concurrent.dll",
+        "virtualPath": "System.Collections.Concurrent.dll"
+      },
+      {
+        "name": "System.Collections.Immutable.dll",
+        "virtualPath": "System.Collections.Immutable.dll"
+      },
+      {
+        "name": "System.ComponentModel.Primitives.dll",
+        "virtualPath": "System.ComponentModel.Primitives.dll"
+      },
+      {
+        "name": "System.Diagnostics.Contracts.dll",
+        "virtualPath": "System.Diagnostics.Contracts.dll"
+      },
+      {
+        "name": "System.IO.Compression.dll",
+        "virtualPath": "System.IO.Compression.dll"
+      },
+      {
+        "name": "System.IO.Pipelines.dll",
+        "virtualPath": "System.IO.Pipelines.dll"
+      },
+      {
+        "name": "System.Linq.dll",
+        "virtualPath": "System.Linq.dll"
+      },
+      {
+        "name": "System.Memory.dll",
+        "virtualPath": "System.Memory.dll"
+      },
+      {
+        "name": "System.Private.CoreLib.dll",
+        "virtualPath": "System.Private.CoreLib.dll"
+      },
+      {
+        "name": "System.Runtime.InteropServices.JavaScript.dll",
+        "virtualPath": "System.Runtime.InteropServices.JavaScript.dll"
+      },
+      {
+        "name": "System.Runtime.dll",
+        "virtualPath": "System.Runtime.dll"
+      },
+      {
+        "name": "System.Text.Encodings.Web.dll",
+        "virtualPath": "System.Text.Encodings.Web.dll"
+      },
+      {
+        "name": "System.Text.Json.dll",
+        "virtualPath": "System.Text.Json.dll"
+      },
+      {
+        "name": "System.Text.RegularExpressions.dll",
+        "virtualPath": "System.Text.RegularExpressions.dll"
+      },
+      {
+        "name": "System.Threading.Channels.dll",
+        "virtualPath": "System.Threading.Channels.dll"
+      }
+    ],
+    "icu": [
+      {
+        "name": "icudt.dat",
+        "virtualPath": "icudt.dat"
+      },
+      {
+        "name": "icudt_CJK.dat",
+        "virtualPath": "icudt_CJK.dat"
+      },
+      {
+        "name": "icudt_EFIGS.dat",
+        "virtualPath": "icudt_EFIGS.dat"
+      },
+      {
+        "name": "icudt_no_CJK.dat",
+        "virtualPath": "icudt_no_CJK.dat"
+      }
+    ]
+  }
+};
